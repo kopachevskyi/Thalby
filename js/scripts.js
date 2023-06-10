74f9d1fce5c6a666953401e12f108ef2391c831e
kopachevskyi <sashakopachevskyi@gmail.com>
2023-06-10 22:58:02 +0200

    Thalby 2 sections

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         nav:true,
-        //autoplay:true,
-        //autoplayTimeout:2000,
+        autoplay:true,
+        autoplayTimeout:2000,
         navText: [ '', ' ' ],
 
         responsive:{
